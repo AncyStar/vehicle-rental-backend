a3965db6b278ef95fed19d8f689fdc802faf796e
AncyStar <ancystarz@gmail.com>
2025-02-28 23:09:50 +0530

    dw

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const paymentRoutes = require("./routes/payments");
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/authentication", authenRoutes);
