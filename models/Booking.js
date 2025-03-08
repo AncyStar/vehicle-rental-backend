ab53a1ae1c49ba06b9038586366afa067a8d5922
AncyStar <ancystarz@gmail.com>
2025-03-08 19:44:50 +0530

    njn

--- models/Booking.js
+++ models/Booking.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const BookingSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  vehicle: {
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  vehicleId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Vehicle",
     required: true,
