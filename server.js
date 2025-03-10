dfaea14582b6786b6ee32e3c67379a19ebac8363
AncyStar <ancystarz@gmail.com>
2025-03-10 18:29:28 +0530

    commit

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.get("/", (req, res) => {
   res.send("Vehicle Rental API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
