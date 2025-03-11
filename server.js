68d02d96032c77edcee5058fb5259e861882be05
AncyStar <ancystarz@gmail.com>
2025-03-11 16:48:31 +0530

    zc

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: "https://vehicle-rental-frontend.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
